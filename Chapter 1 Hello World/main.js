69e813738e302f5c0ef53647a5f7133bd7bd6378
Ninedeadeyes <tommykwong_84@hotmail.com>
2022-01-17 23:55:10 +0000

    1st Chapter 'Hello World' Notes

---
+++ Chapter 1 Hello World/main.js
@@ -0,0 +1,18 @@
+
+alert("Hello World");   //This is a simple JavaScript Statement 
+                        // A statement is a logical set of instructions that tell your 
+                        // browser what to do. An application will have many statements
+                        
+/* You can tell something is a statement by looking at the last character in it, 
+ it is a semi-colon 
+
+  'Alert' is something known as a function. A function is a reusable chunk of code that 
+ does soemething that is defined by you. 
+
+
+ Whenever you are dealing with text( more commonly known as strings)you will wrap them
+ inside a single quote or a double quote. 
+ 
+ */
+
+ 
