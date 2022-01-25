1a16d4988f353104c9bc02862b6d514c1f09576b
Ninedeadeyes <tommykwong_84@hotmail.com>
2022-01-25 23:00:55 +0000

    Add files via upload

---
+++ Chapter 6 Commenting  your code/main.js
@@ -0,0 +1,20 @@
+/* Comments are the thing we write as part of our code to communicate something to humans
+
+// = For a single line 
+
+*/    
+
+// /* +  */ For for mutli-line commment as shown above  
+
+
+// We can use comment to mark lines of code that we don't want to execute 
+
+// You can use JSdoc to easier communicate what your code does  ( go to their websiste for more info)
+// https://jsdoc.app/about-getting-started.html  
+
+// Commenting Best Practise 
+
+// Always comment your code as you are writing it
+// Use more English and less Javascript 
+//Embrace white space (Want to ensure your code stands out and are clear to follow 
+// Dont comment obvious things, invest that time in commenting the less obvious parts 
